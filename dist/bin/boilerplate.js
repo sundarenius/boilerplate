779c8a0e2e5f19b036a5da14314899c7e3cb000f
Håkan Sundström <sundarenius@gmail.com>
2021-07-07 01:25:21 +0200

    update

--- dist/bin/boilerplate.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("module-alias/register");
-require("source-map-support/register");
-var main_1 = require("../src/main");
-var arg = process.argv[2];
-main_1.start(arg);
--- dist/src/types/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.X = void 0;
-var X;
-(function (X) {
-    X["TEST"] = "test";
-})(X = exports.X || (exports.X = {}));
