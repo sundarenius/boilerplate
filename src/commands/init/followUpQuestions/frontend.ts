0e0ae1e2331a8fd662500aa7bb1606e0f58deda4
Håkan Sundström <sundarenius@gmail.com>
2021-07-07 22:17:01 +0200

    text

--- src/commands/init/followUpQuestions/frontend.ts
+++ src/commands/init/followUpQuestions/frontend.ts
@@ -5,7 +5,7 @@ import { terminalQandA } from '@/utils/helpers';
  * Questions for frontend:
  * 1. appType(spa/ssr)
  * 2. language
- * 3. routing (if SPA, if SSR it's there's need to ask)
+ * 3. routing (if SPA, if SSR then there's need to ask)
  * 4. eslint
  */
 
