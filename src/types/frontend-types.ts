eaeca00e5aecf9a77143f0bcebb0105ee934ce37
Håkan Sundström <sundarenius@gmail.com>
2021-07-10 01:44:12 +0200

    define paths

--- src/types/frontend-types.ts
+++ src/types/frontend-types.ts
@@ -138,3 +138,24 @@ export enum Themes {
 }
 
 export type FrontendFrameworks = ProjectTypes.REACT|ProjectTypes.VUE
+
+export interface Paths {
+  src: string,
+  index: string,
+  app: string,
+  unitConfig: string,
+  components: string,
+  menu: string,
+  footer: string,
+  sidebar: string,
+  pages: string,
+  router: string,
+  state: string,
+  store: string,
+  styles: string,
+  types: string,
+  globals: string,
+  utils: string,
+  helpers: string,
+  e2e: string,
+}
